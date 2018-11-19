8b5399e802356285d517077ef8a106419a6d78a3
Rigellute <rigellute@gmail.com>
2018-11-19 12:30:12 +0000

    init commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::io::{stdin, stdout, Write};
+use std::process::{Command};
+
+fn main() {
+    loop {
+        print!("> ");
+        stdout().flush().unwrap();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let command = input.trim();
+
+        let mut child = Command::new(command).spawn().unwrap();
+
+        child.wait().unwrap();
+    }
+}
